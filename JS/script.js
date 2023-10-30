d149167dd4a64169f265f2beacde80b9aa1da179
carlomarcon <carlomarcon1998@gmail.com>
2023-10-30 17:17:24 +0100

    ADD: BONUS JS

--- JS/script.js
+++ JS/script.js
@@ -2,17 +2,24 @@ const box = document.querySelector(".box");
 const button = document.querySelector("button");
 const h1 = document.querySelector("h1");
 const cellBomb = [];
+const bombClicked = [];
+const Points = [];
+const h2 = document.querySelector("h2");
+const Wrapper = document.querySelector(".wrapper");
+
 // EVOCARE FUNZIONE
 
 button.addEventListener("click", function () {
   box.style.display = "flex";
   h1.style.display = "none";
+  h2.style.display = "inline-block";
 
   const difficulty = document.querySelector("select").value;
 
   generator(difficulty);
   Listener(difficulty);
-  numberBomb(16);
+  numberBomb(4);
+  console.log(cellBomb);
 });
 
 // FUNZIONI
@@ -37,10 +44,32 @@ function generator(numberofcell) {
 
 function Listener(numberofcell) {
   let cell = document.querySelectorAll(".cell");
-  for (i = 1; i < numberofcell; i++) {
+
+  for (i = 0; i < numberofcell; i++) {
     cell[i].addEventListener("click", function () {
-      this.style.backgroundColor = "blue";
-      console.log(this.innerHTML);
+      if (bombClicked.length === cellBomb.length - 1) {
+        for (let j = 0; j < cellBomb.length; j++) {
+          cell[cellBomb[j] - 1].style.backgroundColor = "red";
+        }
+        button.addEventListener("click", function () {
+          location.reload();
+        });
+      } else if (
+        cellBomb.includes(parseInt(this.innerHTML)) === false &&
+        bombClicked.length < cellBomb.length
+      ) {
+        this.style.backgroundColor = "blue";
+        Points.push(parseInt(this.innerHTML));
+      } else if (
+        cellBomb.includes(parseInt(this.innerHTML)) === true &&
+        bombClicked.length < cellBomb.length
+      ) {
+        this.style.backgroundColor = "red";
+        bombClicked.push(parseInt(this.innerHTML));
+        console.log(bombClicked);
+      }
+      document.querySelector("h2").innerHTML =
+        `Il tuo punteggio è di ` + Points.length + ` su 100.`;
     });
   }
 }
